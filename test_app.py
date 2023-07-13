fb13bc39af7091c53e3398300fd1113def7420ed
Vicky <vignesh26.manoharan@gmail.com>
2023-07-13 11:39:07 +0530

    New

--- test_app.py
+++ test_app.py
@@ -17,38 +17,6 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data.decode(), 'This is the about page.')
 
-    # def test_user(self):
-    #     # Test with a regular username
-    #     username = 'John'
-    #     response = self.app.get(f'/user/{username}')
-    #     self.assertEqual(response.status_code, 200)
-    #     expected_message = f"Welcome, {username}!"
-    #     self.assertEqual(response.data.decode(), expected_message)
-
-    #     # Test with a username containing special characters
-    #     special_username = '@#User123!'
-    #     response = self.app.get(f'/user/{special_username}')
-    #     self.assertEqual(response.status_code, 200)
-    #     expected_message = f"Welcome, {special_username}!"
-    #     self.assertEqual(response.data.decode(), expected_message)
-
-    # def test_multiply(self):
-    #     # Test with positive numbers
-    #     num1 = 5
-    #     num2 = 10
-    #     response = self.app.get(f'/multiply/{num1}/{num2}')
-    #     self.assertEqual(response.status_code, 200)
-    #     expected_message = f"The multiplication of {num1} and {num2} is: 50"
-    #     self.assertEqual(response.data.decode(), expected_message)
-
-    #     # Test with negative numbers
-    #     num1 = -5
-    #     num2 = 10
-    #     response = self.app.get(f'/multiply/{num1}/{num2}')
-    #     self.assertEqual(response.status_code, 200)
-    #     expected_message = f"The multiplication of {num1} and {num2} is: -50"
-    #     self.assertEqual(response.data.decode(), expected_message)
-
     def test_capitalize(self):
         # Test with a lowercase text
         text = 'hello, world!'
@@ -69,11 +37,13 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 404)
         self.assertIn(b'Not Found', response.data)
 
+
 if __name__ == '__main__':
+    # Create a test suite
+    suite = unittest.TestLoader().loadTestsFromTestCase(FlaskTestCase)
+
     # Configure the test runner with XML output
     runner = xmlrunner.XMLTestRunner(output='xunit-reports')
 
-    # Discover and run the tests with the configured runner
-    unittest.main(testRunner=runner)
-
-# sonar-scanner.bat -D"sonar.projectKey=flask" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=sqp_8549c4135311801b29a82a33d0826344bfe38c71" -D"sonar.python.coverage.reportPaths=coverage.xml" -D"sonar.tests=." -D"sonar.test.inclusions=test_*.py" -D"sonar.python.xunit.reportPath=xunit-reports/*.xml"
+    # Run the test suite with the configured runner
+    result = runner.run(suite)
